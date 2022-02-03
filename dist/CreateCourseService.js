a5cd9bb0bf8555aff560dc19a2b21a3e7408d57a
Eldon de Aquino Costa <eldon.costa@compasso.com.br>
2022-02-03 03:21:01 -0300

    remove node_modules

--- dist/CreateCourseService.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateCourseService = void 0;
-class CreateCourseService {
-    execute(name, duration, educator) {
-        console.log(name, duration, educator);
-    }
-}
-exports.CreateCourseService = CreateCourseService;
